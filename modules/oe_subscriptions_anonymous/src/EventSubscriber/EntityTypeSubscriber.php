50aa58d429fca5aa097d399717caf8845e08d673
AaronGilMartinez <aaron.gil.martinez@nttdata.com>
2023-10-17 11:08:59 +0200

    OEL-2669: Move config event subscriber to entity hooks, as use entity type trait.

--- modules/oe_subscriptions_anonymous/src/EventSubscriber/EntityTypeSubscriber.php
+++ modules/oe_subscriptions_anonymous/src/EventSubscriber/EntityTypeSubscriber.php
@@ -4,15 +4,13 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_subscriptions_anonymous\EventSubscriber;
 
-use Drupal\Core\Config\ConfigCrudEvent;
-use Drupal\Core\Config\ConfigEvents;
 use Drupal\Core\Entity\EntityTypeEventSubscriberTrait;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\extra_field\Plugin\ExtraFieldDisplayManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Event subscriber for any config change. Maybe is too much.
+ * Event subscriber for any config change.
  */
 class EntityTypeSubscriber implements EventSubscriberInterface {
 
@@ -40,24 +38,7 @@ class EntityTypeSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents(): array {
     $events = self::getEntityTypeEvents();
-    $events[ConfigEvents::SAVE][] = 'onConfigChange';
-    $events[ConfigEvents::DELETE][] = 'onConfigChange';
     return $events;
-  }
-
-  /**
-   * Flush cache on entity type creation.
-   *
-   * * @param \Drupal\Core\Config\ConfigCrudEvent $event
-   *   The event object.
-   */
-  public function onConfigChange(ConfigCrudEvent $event): void {
-    $config = $event->getConfig();
-    // Flag config that starts with 'subscribe_'.
-    if (!empty($config->get('flag_type')) && str_starts_with($config->get('id'), 'subscribe_')) {
-      // Clear extra fields definitions cache.
-      $this->extraFieldManager->clearCachedDefinitions();
-    }
   }
 
   /**
