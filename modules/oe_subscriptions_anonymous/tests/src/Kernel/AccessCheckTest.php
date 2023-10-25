2d398a4a7f35e9db93b275ec147873759841796e
AaronGilMartinez <aaron.gil.martinez@nttdata.com>
2023-10-25 17:21:18 +0200

    OEL-2673: Replace access_check service for access_manager in test.

--- modules/oe_subscriptions_anonymous/tests/src/Kernel/AccessCheckTest.php
+++ modules/oe_subscriptions_anonymous/tests/src/Kernel/AccessCheckTest.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_subscriptions_anonymous\Kernel;
 
-use Drupal\Core\Routing\RouteMatch;
 use Drupal\entity_test\Entity\EntityTestBundle;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\node\Entity\Node;
@@ -46,10 +45,10 @@ class AccessCheckTest extends KernelTestBase {
     $this->installEntitySchema('user');
     $this->installEntitySchema('node');
     $this->installEntitySchema('flagging');
+    $this->installEntitySchema('message');
     $this->installSchema('system', ['sequences']);
     $this->installSchema('flag', ['flag_counts']);
     $this->installConfig(['filter', 'flag', 'message_subscribe']);
-    $this->installEntitySchema('message');
 
     // Create a test bundle to use as referenced bundle.
     EntityTestBundle::create(['id' => 'article'])->save();
@@ -60,23 +59,18 @@ class AccessCheckTest extends KernelTestBase {
    */
   public function testAnonymousLink(): void {
     // Services a variables.
-    $access_checker = \Drupal::service('oe_subscriptions_anonymous.access_checker');
-    $route_provider = \Drupal::service('router.route_provider');
     $route_name = 'oe_subscriptions_anonymous.anonymous_subscribe';
-    $route = $route_provider->getRouteByName($route_name);
     // Create a flag.
+    $flag_id = 'subscribe_article';
     $flag = $this->createFlagFromArray([
-      'id' => 'subscribe_article',
-      'label' => 'Subscribe article',
+      'id' => $flag_id,
       'flag_type' => $this->getFlagType('node'),
       'entity_type' => 'node',
       'bundles' => ['article'],
     ]);
-    $flag_id = $flag->id();
     // A flag that applies to all bundles.
     $this->createFlagFromArray([
       'id' => 'another_flag',
-      'label' => 'Another flag',
       'flag_type' => $this->getFlagType('entity_test_with_bundle'),
       'entity_type' => 'entity_test_with_bundle',
       'bundles' => [],
@@ -84,95 +78,58 @@ class AccessCheckTest extends KernelTestBase {
     // Node.
     $node = Node::create([
       'title' => $this->randomMachineName(8),
-      'body' => [
-        [
-          'value' => $this->randomMachineName(32),
-          'format' => filter_default_format(),
-        ],
-      ],
       'type' => 'article',
       'status' => 1,
     ]);
     $node->save();
     $nid = $node->id();
-
-    // No paramenters.
-    $route_match = new RouteMatch(
-      $route_name,
-      $route,
-    );
-    $this->assertFalse($access_checker->access($route_match)->isAllowed());
-
+    $access_manager = $this->container->get('access_manager');
     // Empty.
-    $route_match = new RouteMatch(
-      $route_name,
-      $route,
+    $this->assertFalse($access_manager->checkNamedRoute($route_name,
       [
         'flag' => '',
         'entity_id' => '',
       ]
-    );
-    $this->assertFalse($access_checker->access($route_match)->isAllowed());
-
+    ));
     // No matching flag.
-    $route_match = new RouteMatch(
-      $route_name,
-      $route,
+    $this->assertFalse($access_manager->checkNamedRoute($route_name,
       [
         'flag' => 'subscribe_events',
         'entity_id' => $nid,
       ]
-    );
-    $this->assertFalse($access_checker->access($route_match)->isAllowed());
-
+    ));
     // Flag doesn't starts with 'subscribe_'.
-    $route_match = new RouteMatch(
-      $route_name,
-      $route,
+    $this->assertFalse($access_manager->checkNamedRoute($route_name,
       [
         'flag' => 'another_flag',
         'entity_id' => $nid,
       ]
-    );
-    $this->assertFalse($access_checker->access($route_match)->isAllowed());
-
+    ));
     // Disabled flag.
     $flag->disable();
     $flag->save();
-    $route_match = new RouteMatch(
-      $route_name,
-      $route,
+    $this->assertFalse($access_manager->checkNamedRoute($route_name,
       [
         'flag' => $flag_id,
         'entity_id' => $nid,
       ]
-    );
-    $this->assertFalse($access_checker->access($route_match)->isAllowed());
+    ));
     $flag->enable();
     $flag->save();
-
     // Not existing node.
-    $route_match = new RouteMatch(
-      $route_name,
-      $route,
+    $this->assertFalse($access_manager->checkNamedRoute($route_name,
       [
         'flag' => $flag_id,
-        'entity_id' => 1234,
+        'entity_id' => '1234',
       ]
-    );
-    $this->assertFalse($access_checker->access($route_match)->isAllowed());
-
+    ));
     // Finally a subscribe parameters matching all conditions.
-    $route_match = new RouteMatch(
-      $route_name,
-      $route,
+    $this->assertFalse($access_manager->checkNamedRoute($route_name,
       [
         'flag' => $flag_id,
         'entity_id' => $nid,
       ]
-    );
-    $this->assertTrue($access_checker->access($route_match)->isAllowed());
-
+    ));
   }
 
 }
