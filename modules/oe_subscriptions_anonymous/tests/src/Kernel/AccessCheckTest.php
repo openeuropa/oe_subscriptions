6912cf5e87c109a332906ad3137445b13b2ccb2b
AaronGilMartinez <aaron.gil.martinez@nttdata.com>
2023-10-31 10:17:33 +0100

    OEL-2673: Create helper function to perform the access checks.

--- modules/oe_subscriptions_anonymous/tests/src/Kernel/AccessCheckTest.php
+++ modules/oe_subscriptions_anonymous/tests/src/Kernel/AccessCheckTest.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_subscriptions_anonymous\Kernel;
 
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Session\AnonymousUserSession;
 use Drupal\entity_test\Entity\EntityTestBundle;
 use Drupal\KernelTests\KernelTestBase;
@@ -42,6 +43,13 @@ class AccessCheckTest extends KernelTestBase {
   ];
 
   /**
+   * The access manager service.
+   *
+   * @var \Drupal\Core\Access\AccessManagerInterface
+   */
+  protected $accessManager;
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp(): void {
@@ -58,14 +66,14 @@ class AccessCheckTest extends KernelTestBase {
 
     // Create a test bundle to use as referenced bundle.
     EntityTestBundle::create(['id' => 'article'])->save();
+    // Get access manager service.
+    $this->accessManager = $this->container->get('access_manager');
   }
 
   /**
    * Tests the access to the link based on subscription_id parameters.
    */
   public function testAnonymousLinkAccess(): void {
-    // Services a variables.
-    $route_name = 'oe_subscriptions_anonymous.anonymous_subscribe';
     // Create a flag.
     $flag_id = 'subscribe_article';
     $flag = $this->createFlagFromArray([
@@ -86,101 +94,54 @@ class AccessCheckTest extends KernelTestBase {
       'type' => 'article',
       'status' => 1,
     ]);
-    $node->save();
     $nid = $node->id();
-    $access_manager = $this->container->get('access_manager');
     // Give access content permission to anonymous.
     $this->grantPermissions(Role::load('anonymous'), ['access content']);
     $this->setCurrentUser(new AnonymousUserSession());
 
     // Empty parameter values.
-    $access = $access_manager->checkNamedRoute(
-      $route_name,
-      [
-        'flag' => '',
-        'entity_id' => '',
-      ],
-      NULL,
-      TRUE,
-    );
-    $this->assertFalse($access->isAllowed());
+    $this->assertFalse($this->assertRouteAccess('', ''));
 
     // No matching flag.
-    $access = $access_manager->checkNamedRoute(
-      $route_name,
-      [
-        'flag' => 'subscribe_events',
-        'entity_id' => $nid,
-      ],
-      NULL,
-      TRUE,
-    );
-    $this->assertFalse($access->isAllowed());
+    $this->assertFalse($this->assertRouteAccess('subscribe_events', $nid));
 
     // Flag doesn't starts with 'subscribe_'.
-    $access = $access_manager->checkNamedRoute(
-      $route_name,
-      [
-        'flag' => 'another_flag',
-        'entity_id' => $nid,
-      ],
-      NULL,
-      TRUE,
-    );
-    $this->assertFalse($access->isAllowed());
+    $this->assertFalse($this->assertRouteAccess('another_flag', $nid));
 
     // Disabled flag.
     $flag->disable();
     $flag->save();
-    $access = $access_manager->checkNamedRoute(
-      $route_name,
-      [
-        'flag' => $flag_id,
-        'entity_id' => $nid,
-      ],
-      NULL,
-      TRUE,
-    );
-    $this->assertFalse($access->isAllowed());
+    $this->assertFalse($this->assertRouteAccess($flag_id, $nid));
     $flag->enable();
     $flag->save();
 
     // Not existing node.
-    $access = $access_manager->checkNamedRoute(
-      $route_name,
-      [
-        'flag' => $flag_id,
-        'entity_id' => '1234',
-      ],
-      NULL,
-      TRUE,
-    );
-    $this->assertFalse($access->isAllowed());
+    $this->assertFalse($this->assertRouteAccess($flag_id, '1234'));
 
     // Finally, subscription parameters matching all conditions.
-    $access = $access_manager->checkNamedRoute(
-      $route_name,
-      [
-        'flag' => $flag_id,
-        'entity_id' => $nid,
-      ],
-      NULL,
-      TRUE,
-    );
-    $this->assertTrue($access->isAllowed());
+    $this->assertTrue($this->assertRouteAccess($flag_id, $nid));
 
     // Route is not allowed for logged users.
-    $access = $access_manager->checkNamedRoute(
+    $this->assertFalse($this->assertRouteAccess($flag_id, $nid, $this->createUser([], 'logged_user')));
+
+  }
+
+  /**
+   * Helper function to perform checks with access_manager service.
+   */
+  protected function assertRouteAccess(string $flag_id, string $entity_id, AccountInterface $user = NULL) {
+    $route_name = 'oe_subscriptions_anonymous.anonymous_subscribe';
+    $access_check = $this->accessManager->checkNamedRoute(
       $route_name,
       [
         'flag' => $flag_id,
-        'entity_id' => $nid,
+        'entity_id' => $entity_id,
       ],
-      $this->createUser([], 'logged_user'),
+      $user,
       TRUE,
     );
-    $this->assertFalse($access->isAllowed());
 
+    return $access_check->isAllowed();
   }
 
 }
