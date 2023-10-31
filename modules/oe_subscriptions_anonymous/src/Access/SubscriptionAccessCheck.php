f9e6f48279b5b9b81f465a56e67eaa70190ca656
Francesco Sardara <francesco@tdgwebservices.com>
2023-10-31 18:01:44 +0100

    OEL-2673: Check if loaded entity is flaggable.

--- modules/oe_subscriptions_anonymous/src/Access/SubscriptionAccessCheck.php
+++ modules/oe_subscriptions_anonymous/src/Access/SubscriptionAccessCheck.php
@@ -47,7 +47,23 @@ class SubscriptionAccessCheck implements AccessInterface {
 
     $entity = $this->flagService->getFlaggableById($flag, $entity_id);
     if (empty($entity)) {
-      return AccessResult::forbidden()->addCacheableDependency($flag);
+      return AccessResult::forbidden('No flaggable entity found.')->addCacheableDependency($flag);
+    }
+
+    // The flag module doesn't expose a method to check if an entity can be
+    // flagged with a specific flag. This check is done only during the
+    // \Drupal\flag\FlagServiceInterface::flag() (or unflag) execution.
+    // The flag route is protected anyway because it requires a CSRF token, that
+    // is generated when a link to the route is outputted (e.g. by the action
+    // link plugins) and said links are outputted only when the entity is
+    // flaggable. In our scenario we don't need a CSRF token as there's no
+    // direct action when visiting the link, so we need to check the bundles
+    // by ourselves.
+    // We cannot use the flag access service either, as we are not configuring
+    // anonymous users to flag content.
+    $bundles = $flag->getBundles();
+    if (!empty($bundles) && !in_array($entity->bundle(), $bundles)) {
+      return AccessResult::forbidden('The flag does not apply to the bundle of the entity')->addCacheableDependency($flag);
     }
 
     return $entity->access('view', NULL, TRUE)->addCacheableDependency($flag);
