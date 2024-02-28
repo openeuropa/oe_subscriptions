3e400ac890e90e6fdbaf1b8f33dfceb30e08ead5
AaronGilMartinez <aaron.gil.martinez@nttdata.com>
2024-02-28 17:26:10 +0100

    OEL-2840: Remove extra spaces.

--- modules/oe_subscriptions_anonymous/src/MailTemplate/UserSubscriptionsAccess.php
+++ modules/oe_subscriptions_anonymous/src/MailTemplate/UserSubscriptionsAccess.php
@@ -56,8 +56,8 @@ class UserSubscriptionsAccess implements ContainerInjectionInterface, MailTempla
     ];
 
     $text = $this->t("You are receiving this e-mail because you requested access to your subscriptions page on @site_url. \r\n
-    Click the following link to access your subscriptions page: @subscriptions_page_link \r\n
-    If you didn't request access to your subscriptions page or you're not sure why you received this e-mail, you can delete it.", $variables);
+Click the following link to access your subscriptions page: @subscriptions_page_link \r\n
+If you didn't request access to your subscriptions page or you're not sure why you received this e-mail, you can delete it.", $variables);
 
     $message['subject'] .= $this->t('Access your subscriptions page on @site_url', ['@site_url' => $site_url]);
     $message['body'][] = MailFormatHelper::htmlToText($text);
